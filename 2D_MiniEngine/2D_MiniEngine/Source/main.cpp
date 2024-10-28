47a10805ecacfb83b02de165ad134263701a409b
Nicolas Daniele <nicolasdaniele@ravegan.com>
2024-10-27 23:35:07 -0300

    Added InputManager class

--- 2D_MiniEngine/2D_MiniEngine/Source/main.cpp
+++ 2D_MiniEngine/2D_MiniEngine/Source/main.cpp
@@ -1,5 +1,7 @@
 #include <./SFML/Graphics.hpp>
+#include <stdio.h>
 #include "SpriteRendererComponent.h"
+#include "InputManager.h"
 
 int main()
 {
@@ -12,14 +14,44 @@ int main()
     spriteRenderer->SetTexture("Assets/Sprites/enemy.png");
     spriteRenderer->SetScale(3.f, 3.f);
 
+    InputManager* inputManager = new InputManager();
+
     while (window.isOpen())
     {
-        sf::Event event;
+        /*sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+        }*/
+
+        inputManager->Update();
+
+
+        if (inputManager->inputState.left)
+        {
+            printf("LEFT IS PRESSED\n");
         }
+        if (inputManager->inputState.right)
+        {
+            printf("RIGHT IS PRESSED\n");
+        }
+        if (inputManager->inputState.up)
+        {
+            printf("UP IS PRESSED\n");
+        }
+        if (inputManager->inputState.down)
+        {
+            printf("DOWN IS PRESSED\n");
+        }
+        if (inputManager->inputState.escape)
+        {
+            printf("ESCAPE IS PRESSED\n");
+            window.close();
+        }
+
+
+
 
         window.clear();
         spriteRenderer->Update();
@@ -27,6 +59,7 @@ int main()
     }
 
     delete spriteRenderer;
+    delete inputManager;
 
     return 0;
 }
